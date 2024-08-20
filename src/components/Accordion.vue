ec187d708b44fb8d50bf27001b6651801cd6c845
KristinaCh <chashchina.k@arbis29.ru>
2024-08-20 21:15:04 +0300

    Аккордион на vue

---
+++ src/components/Accordion.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="accordion">
+    <slot></slot>
+  </div>
+</template>
+  
+  <script>
+  export default {
+    name: 'Accordion'
+  }
+  </script>
+  
+  <style scoped>
+  .accordion {
+    width: 100%;
+  }
+  </style>
+  
